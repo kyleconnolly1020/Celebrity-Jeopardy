e9abefb202984b8796eb91a6703d068ce1da1261
buzzhaha <boming.fan@gmail.com>
2018-01-23 14:36:49 -0800

    get

--- public/js/jeopardy.js
+++ public/js/jeopardy.js
@@ -1,7 +1,25 @@
-$("button").click(function(){
-    var queryURL = "http://jservice.io/api/clues?category=" + this.id;
-    $.get(queryURL, function(data, status){
-        console.log(data);
-        // console.log("Data: " + data + "\nStatus: " + status);
+
+$(document).ready(function () {
+
+    $("button").click(function () {
+        var queryURL = "http://jservice.io/api/clues?category=" + this.id;
+        $.get(queryURL, function (data, status) {
+            console.log(data);
+            // console.log("Data: " + data + "\nStatus: " + status);
+        });
     });
+
+
+    $(".waves-effect").click(function (event) {
+        event.preventDefault();
+        var queryURL = "http://jservice.io/api/clues?category=" + this.id;
+        console.log(this.id);
+        $.get(queryURL, function (data, status) {
+            console.log(data);
+            // console.log("Data: " + data + "\nStatus: " + status);
+        });
+    });
+
+
+
 });
