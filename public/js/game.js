b37aa7940771e8f2936098c17f2a46a58b4cb9a0
buzzhaha <boming.fan@gmail.com>
2018-01-24 14:20:20 -0800

    questionvalidation

--- public/js/game.js
+++ public/js/game.js
@@ -5,8 +5,32 @@ $(function () {
     });
  
 
+// Question validations
 
-    
+var count = 0;
+    $('#modal1').modal();
+    $("#sub-btn1").click(function () {
+        var text = $("#answer1").val();
+        var answer = $("#q-200").data("value");
+        var value = $("#q-200").val();
+        console.log("worked" + text);
+        console.log("worked" + answer);
+        console.log(value);
+        $("#q-200").removeClass("modal-trigger");
+        if (text === answer) {
+            console.log("if working");
+            count += parseInt($("#q-200").val());
+            console.log(count);
+        }
+        
+
+    })
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+        window.onbeforeunload = function() {
+    return "submit";
+}
+
+
 
 });
 
