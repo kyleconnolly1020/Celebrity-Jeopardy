00c0709b562ca770a9d975b216446149e640c9c4
ematt44 <ericmatthews133@gmail.com>
2018-01-24 10:41:45 -0800

    modal

--- public/js/jeopardy.js
+++ public/js/jeopardy.js
@@ -4,4 +4,21 @@ $("button").click(function(){
         console.log(data);
         // console.log("Data: " + data + "\nStatus: " + status);
     });
+
+
+
+    (function ($) {
+        $(function () {
+    
+            //initialize all modals           
+            $('.modal').modal();
+    
+            //now you can open modal from code
+            $('#modal1').modal('open');
+    
+            //or by click on trigger
+            $('.trigger-modal').modal();
+    
+        }); // end of document ready
+    })(jQuery); // end of jQuery name space
 });
